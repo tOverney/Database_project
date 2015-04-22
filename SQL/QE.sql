4527c7ab19dd6d76228c1db0e0c5726b371da508
varrieta <valentine.arrieta@epfl.ch>
2015-04-22 12:55:51 +0200

    SQL Queries for the 2nd deliverable

---
+++ SQL/QE.sql
@@ -0,0 +1,5 @@
+﻿-- Compute the min, max and average height of female persons
+
+SELECT DISTINCT MIN(height) as min_height, MAX(height) as max_height, AVG(height) as avg_height
+FROM person
+WHERE gender = 'f'
