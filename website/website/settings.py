9214e592479bab9c5242564013a01c54d48fbb97
Tristan Overney <tristan.overney@epfl.ch>
2015-04-25 13:23:31 +0200

    some more config and stubs

--- website/website/settings.py
+++ website/website/settings.py
@@ -31,12 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = (
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
+    'application',
 )
 
 MIDDLEWARE_CLASSES = (
